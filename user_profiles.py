6a3396a833d7ececc17356da0e88ae439cc83518
Deep Monapara <118141711+deepmonapara9@users.noreply.github.com>
2025-08-12 18:45:15 +0530

    30254

---
+++ user_profiles.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    """Extend user profiles storing additonal personal details. """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
