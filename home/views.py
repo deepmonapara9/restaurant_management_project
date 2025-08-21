38fa06f9a6b3e9a85e6e7a71bec10c45dacde779
Deep Monapara <118141711+deepmonapara9@users.noreply.github.com>
2025-08-21 17:49:05 +0530

    37143

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
+def home(request):
+    """
+    Home page that fetches the restaurant details
+    and display the details.
+    """
+    restaurant = Restaurant.objects.first()
+    return render(request, "homepage.html", {"restaurant": restaurant})
