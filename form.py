c7fc9766cdd22d8b37f70cd75a9a5fa29b3f6c4c
Deep Monapara <118141711+deepmonapara9@users.noreply.github.com>
2025-08-21 18:20:39 +0530

    37140

--- form.py
+++ form.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Contact
 
 class ContactForm(form.ModelForm):
+    name = forms.CharField(max_length=200, label="Deep")
+    email = forms.EmailField(label="abc@gmail.com")
+    message = forms.CharField(widget=forms.Textarea, label="Yello!")
+
     class Meta:
         model = Contact
         fields = ['name', 'email']
