1663aca4c3651cd5e5246ee1e3901e56735448ee
Deep Monapara <118141711+deepmonapara9@users.noreply.github.com>
2025-08-21 17:59:01 +0530

    37142

--- home/models.py
+++ home/models.py
@@ -9,6 +9,7 @@ class RestaurantLocation(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
+    opening_hours = models.JSONField(max_length=100)
 
     def __str__(self):
         return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
