692c4328a43e7ca40c707a0c1262ee5a12241857
Deep Monapara <118141711+deepmonapara9@users.noreply.github.com>
2025-08-21 18:30:02 +0530

    37140

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from django.conf import settings
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
@@ -9,3 +12,40 @@ def home(request):
     """
     restaurant = Restaurant.objects.first()
     return render(request, "homepage.html", {"restaurant": restaurant})
+
+
+def contact(request):
+    """
+    Handle the contact form submission and send email notifications.
+    """
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # extract form data
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+
+            # email subject & body
+            subject = f"New contact form mesage from {name}"
+            body = f"""
+            You have recevied a new mesage from Tasty Bites:
+            Name: {name},
+            Email: {email},
+            Message: {message}
+            """
+
+            # send email
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [settings.CONTACT_EMAIL],
+                fail_silently=False,
+            )
+
+            return redirect("contact_success")
+    else:
+        form = ContactForm()
+
+    return render(request, "contact.html", {"form": form})
